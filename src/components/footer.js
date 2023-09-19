501aa1908dc76cd37369de18b4220fc151362238
ethansjc <ethansjcheong@gmail.com>
2023-09-19 17:18:49 -0400

    created deployment

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,5 @@
 import styles from "../styles/footer.module.css";
 
-const scrollBtn = document.getElementById("scroll-to-top");
-
-// scrollBtn.addEventListener("click", function () {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// });
-
-// window.addEventListener("scroll", function () {
-//   if (window.scrollY > 100) {
-//     scrollBtn.style.display = "block";
-//   } else {
-//     scrollBtn.style.display = "none";
-//   }
-// });
-
 const Footer = () => {
   return (
     <div>
