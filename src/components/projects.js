53712b5d52f7447f76566db458ef750e5a329c69
ethansjc <ethansjcheong@gmail.com>
2023-09-21 15:58:17 -0400

    adjusted sections

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,29 +5,30 @@ const Projects = () => {
   return (
     <div id="projects">
       <div className={styles.container}>
-        <h1 className={styles.h1}>Projects</h1>
-        <div className={styles.cards}>
-          <div className={styles.card}>
-            <a href="https://protech.onrender.com/">
-              <div className={styles.cardImg}>
-                <img src={Protech} alt="img"></img>
-              </div>
-              <h3 className={styles.h3}>ProTech</h3>
-            </a>
-            <h6 className={styles.h6}>Ecommerce website using MERN stack</h6>
-          </div>
-          <div className={styles.card}>
-            <a href="https://github.com/ethansjc/AWS-Projects">
-              <div className={styles.cardImg}>
-                <img src={AWSProject} alt="img"></img>
-              </div>
-              <h3 className={styles.h3}>AWS Project Series</h3>
-            </a>
-            <h6 className={styles.h6}>
-              Tutorial guide on using aws various services
-            </h6>
-          </div>
-          {/* <div className={styles.card}>
+        <div className={styles.projectsContainer}>
+          <h1 className={styles.h1}>Projects</h1>
+          <div className={styles.cards}>
+            <div className={styles.card}>
+              <a href="https://protech.onrender.com/">
+                <div className={styles.cardImg}>
+                  <img src={Protech} alt="img"></img>
+                </div>
+                <h3 className={styles.h3}>ProTech</h3>
+              </a>
+              <h6 className={styles.h6}>Ecommerce website using MERN stack</h6>
+            </div>
+            <div className={styles.card}>
+              <a href="https://github.com/ethansjc/AWS-Projects">
+                <div className={styles.cardImg}>
+                  <img src={AWSProject} alt="img"></img>
+                </div>
+                <h3 className={styles.h3}>AWS Project Series</h3>
+              </a>
+              <h6 className={styles.h6}>
+                Tutorial guide on using aws various services
+              </h6>
+            </div>
+            {/* <div className={styles.card}>
             <div className={styles.cardImg}>
               <img
                 src="../images/deliciousness.jpg"
@@ -39,13 +40,14 @@ const Projects = () => {
             </a>
             <h6>Recipes site using React</h6>
           </div> */}
-          {/* <div className={styles.card}>
+            {/* <div className={styles.card}>
             <div className={styles.cardImg}>
               <img src="../images/proj.jpg" alt="img"></img>
             </div>
             <h3>My Project</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
           </div> */}
+          </div>
         </div>
       </div>
     </div>
--- src/components/skills.js
+++ src/components/skills.js
@@ -3,95 +3,100 @@ import styles from "../styles/skills.module.css";
 const Skills = () => {
   return (
     <div className={styles.container}>
-      <span className={styles.skillsec}>Skills</span>
-      <ul className={styles.skillsList}>
-        <li>
-          <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
-            <img src="https://skillicons.dev/icons?i=html" alt="html"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
-            <img src="https://skillicons.dev/icons?i=js" alt="js"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
-            <img src="https://skillicons.dev/icons?i=css" alt="css"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.typescriptlang.org/docs/">
-            <img src="https://skillicons.dev/icons?i=ts" alt="ts"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://react.dev/">
-            <img src="https://skillicons.dev/icons?i=react" alt="react"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://redux.js.org/">
-            <img src="https://skillicons.dev/icons?i=redux" alt="redux"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://learn.microsoft.com/en-us/dotnet/csharp/">
-            <img src="https://skillicons.dev/icons?i=cs" alt="cs"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://getbootstrap.com/">
-            <img
-              src="https://skillicons.dev/icons?i=bootstrap"
-              alt="bootstrap"
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://learn.microsoft.com/en-us/dotnet/">
-            <img src="https://skillicons.dev/icons?i=dotnet" alt="dotnet" />
-          </a>
-        </li>
-        <li>
-          <a href="https://nodejs.org/en">
-            <img src="https://skillicons.dev/icons?i=nodejs" alt="nodejs"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://expressjs.com/">
-            <img
-              src="https://skillicons.dev/icons?i=expressjs"
-              alt="expressjs"
-            ></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.python.org/">
-            <img src="https://skillicons.dev/icons?i=py" alt="py"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.java.com/en/">
-            <img src="https://skillicons.dev/icons?i=java" alt="java"></img>
-          </a>
-        </li>
-        <li>
-          <a href="https://docs.aws.amazon.com/index.html">
-            <img src="https://skillicons.dev/icons?i=aws" alt="aws" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.mongodb.com/docs/">
-            <img src="https://skillicons.dev/icons?i=mongodb" alt="mongodb" />
-          </a>
-        </li>
-        <li>
-          <a href="https://dev.mysql.com/doc/">
-            <img src="https://skillicons.dev/icons?i=mysql" alt="mysql" />
-          </a>
-        </li>
-      </ul>
+      <div className={styles.skillsContainer}>
+        <h1 className={styles.skillsec}>Skills</h1>
+        <ul className={styles.skillsList}>
+          <li>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
+              <img src="https://skillicons.dev/icons?i=html" alt="html"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+              <img src="https://skillicons.dev/icons?i=js" alt="js"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+              <img src="https://skillicons.dev/icons?i=css" alt="css"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.typescriptlang.org/docs/">
+              <img src="https://skillicons.dev/icons?i=ts" alt="ts"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://react.dev/">
+              <img src="https://skillicons.dev/icons?i=react" alt="react"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://redux.js.org/">
+              <img src="https://skillicons.dev/icons?i=redux" alt="redux"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://learn.microsoft.com/en-us/dotnet/csharp/">
+              <img src="https://skillicons.dev/icons?i=cs" alt="cs"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://getbootstrap.com/">
+              <img
+                src="https://skillicons.dev/icons?i=bootstrap"
+                alt="bootstrap"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://learn.microsoft.com/en-us/dotnet/">
+              <img src="https://skillicons.dev/icons?i=dotnet" alt="dotnet" />
+            </a>
+          </li>
+          <li>
+            <a href="https://nodejs.org/en">
+              <img
+                src="https://skillicons.dev/icons?i=nodejs"
+                alt="nodejs"
+              ></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://expressjs.com/">
+              <img
+                src="https://skillicons.dev/icons?i=expressjs"
+                alt="expressjs"
+              ></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.python.org/">
+              <img src="https://skillicons.dev/icons?i=py" alt="py"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.java.com/en/">
+              <img src="https://skillicons.dev/icons?i=java" alt="java"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://docs.aws.amazon.com/index.html">
+              <img src="https://skillicons.dev/icons?i=aws" alt="aws" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.mongodb.com/docs/">
+              <img src="https://skillicons.dev/icons?i=mongodb" alt="mongodb" />
+            </a>
+          </li>
+          <li>
+            <a href="https://dev.mysql.com/doc/">
+              <img src="https://skillicons.dev/icons?i=mysql" alt="mysql" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
